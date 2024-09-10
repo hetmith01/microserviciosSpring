0ae70f85dc66732356dfa69ea4d2bb5ca513bc9b
hetmith01 <het.hanc.14@gmail.com>
2024-09-10 17:11:21 -0500

    programando la clase StudentController

--- microservice-student/src/main/java/com/microservice/student/service/StudentServiceImpl.java
+++ microservice-student/src/main/java/com/microservice/student/service/StudentServiceImpl.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 public class StudentServiceImpl implements IStudentService {
 
     @Autowired
-    private StudentRepository studentRepository
+    private StudentRepository studentRepository;
 
 
     @Override
