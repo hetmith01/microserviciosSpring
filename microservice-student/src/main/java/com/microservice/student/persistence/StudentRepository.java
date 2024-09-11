bc1cc692222001c2e77d94bc4d11546373ffeb2f
hetmith01 <het.hanc.14@gmail.com>
2024-09-11 16:45:27 -0500

    cada microservicio está funcional

--- microservice-student/src/main/java/com/microservice/student/persistence/StudentRepository.java
+++ microservice-student/src/main/java/com/microservice/student/persistence/StudentRepository.java
@@ -4,11 +4,14 @@ package com.microservice.student.persistence;
 import com.microservice.student.entities.Student;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+
+@Repository
 public interface StudentRepository extends CrudRepository<Student, Long> {
 
-    @Query("SELET s FROM Student s WHERE s.courseId = :idCourse")
+    @Query("SELECT s FROM Student s WHERE s.courseId = :idCourse")
     List<Student> findAllStudent(Long idCourse);
 }
--- microservice-student/src/main/java/com/microservice/student/service/StudentServiceImpl.java
+++ microservice-student/src/main/java/com/microservice/student/service/StudentServiceImpl.java
@@ -3,10 +3,12 @@ package com.microservice.student.service;
 import com.microservice.student.entities.Student;
 import com.microservice.student.persistence.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
+
+@Service
 public class StudentServiceImpl implements IStudentService {
 
     @Autowired
